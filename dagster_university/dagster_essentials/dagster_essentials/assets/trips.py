57cf58f08b0b1cc8d96209408895344f932b493c
djibril968 <djibril.lekan@gmail.com>
2025-06-21 19:01:56 +0300

    update on dagster learning

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -69,3 +69,70 @@ def ai_adoption() -> None:
     else:
         with open(constants.AI_ADOPTION_FILE_PATH, "wb") as output_file:
             output_file.write(response.content)
+
+@dg.asset (
+    deps = ["taxi_trips_file"]
+)
+
+def taxi_trips() -> None:
+    """
+    here we load our taxi trip data into our db, duckdb as in this instance
+    """
+    query = """
+        create or replace table tax_trips as(
+            select
+                VendorID as vendor_id,
+                PULocationID as pickup_zone_id,
+                DOLocationID as dropoff_zone_id,
+                RatecodeID as rate_code_id,
+                payment_type as payment_type,
+                tpep_dropoff_datetime as dropoff_datetime,
+                tpep_pickup_datetime as pickup_datetime,
+                trip_distance as trip_distance,
+                passenger_count as passenger_count,
+                total_amount as total_amount
+            from 'data/raw/taxi_trips_2023-03.parquet'  
+            );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries= 10,
+    )
+    conn.execute(query)
+
+
+
+    @dg.asset(
+        deps = ["taxi_zones_file"]
+         )    
+    def zones() -> None:
+
+        query = """
+            replace or select table zones as(
+                select
+                    LocationID as zone_id,
+                    zone as zone,
+                    borough as borough,
+                    the_geom as geometry
+                from '{constants.TAXI_ZONES_FILE_PATH}'
+
+            );
+        """
+
+        conn = backoff(
+            fn = duckdb.connect,
+            retry_on=(RuntimeError, duckdb.IOException),
+            kwargs={
+                "database": os.getenv("DUCKDB"),
+            },
+            max_retries= 10,
+        )
+        conn.execute(query)
+
+
+
